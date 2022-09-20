815ff9d81c4d13cae9b57a91a8a08aa2432a8a3d
tgmiau <113451244+tgmiau@users.noreply.github.com>
2022-09-20 12:53:48 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+#Hacer programa que pida 3 notas entre 1 y 7 y luego calcule el promedio de esas 3 notas
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
